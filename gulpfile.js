2ee3ca5052f2e09cabbd29f958c1c1ff28180b83
xchiefx <chief_fei@163.com>
2018-08-31 19:48:29 +0800

    未完成版

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,20 @@ var cssnano = require('gulp-cssnano');
 // js压缩
 var min = require('gulp-uglify');
 // 任务
+// 编译sass 压缩 css
 gulp.task('index',function(){
     gulp.src('./src/sass/*.scss').pipe(sass()).pipe(rename({suffix : '.min'})).pipe(cssnano()).pipe(gulp.dest('dist'))
     // 
 })
-
 // 监视
 gulp.task('dog',function(){
-    gulp.watch('./src/sass/*.scss',['index'])
+    gulp.watch(['./src/sass/*.scss','./src/require/*.js'],['index','js'])
+})
+// 压缩JS
+gulp.task('js',function(){
+    gulp.src('./src/require/index.js').pipe(min()).pipe(rename({suffix:".min"})).pipe(gulp.dest("dist"))
+})
+// 监视
+gulp.task('jsdog',function(){
+    gulp.watch('./src/require/*.js',['js'])
 })
